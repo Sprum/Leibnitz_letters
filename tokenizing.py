60328096475c9c93290564ada53d92119f7e5d1c
Sprum <108943595+Sprum@users.noreply.github.com>
2024-04-24 17:25:55 +0200

    started implementing openai api calls

--- tokenizing.py
+++ tokenizing.py
@@ -3,16 +3,17 @@ import tiktoken
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 
-def read_file(doc_path):
+def read_file(doc_path: str) -> str:
     with open(doc_path, "r") as f:
         body = f.read()
     return body
 
-def get_num_tokens(doc_path):
+
+def get_num_tokens(doc_path: str) -> int:
     """
     counts the number of tokens oftext in a given ´txt file
-    :param doc_path:
-    :return:
+    :param doc_path: str
+    :return: int
     """
     body = read_file(doc_path)
     enc_body = encoding.encode(body)
@@ -21,3 +22,6 @@ def get_num_tokens(doc_path):
     return num_tokens
 
 
+if __name__ == "__main__":
+    path = ""
+    pass
