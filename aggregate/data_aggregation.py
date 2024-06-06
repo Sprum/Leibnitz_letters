79d7ce87f34162533a84bfc57bf6d943c31c2653
Sprum <108943595+Sprum@users.noreply.github.com>
2024-06-06 17:26:11 +0200

    implemented function in util to clean up all csvs at once, last modified csv: 13

--- aggregate/data_aggregation.py
+++ aggregate/data_aggregation.py
@@ -1,9 +1,37 @@
 """Module for aggregating the extracted data"""
 from pathlib import Path
+from typing import List
+
+from pandas import DataFrame
+
+import pandas as pd
+
+
+def read_csv(path: str) -> DataFrame:
+    df = pd.read_csv(path)
+    return df
+
 
 # Read in data
 csv_paths = [path for path in Path("../data/per letter").iterdir()]
 csvs = []
 
+
+def aggregate_data(csvs: List[DataFrame]):
+    """Function to aggregate all the data from a given list of DataFrames to one DataFrame, summing up all Counts for
+    unique places"""
+    # Concatenate all DataFrames
+    all_data = pd.concat(csvs)
+    # Sum up all counts by index (Place)
+    all_data = all_data.groupby("Place").sum().reset_index()
+    return all_data
+
+
 if __name__ == "__main__":
-    print(csv_paths)
+    for csv in csv_paths:
+        df = read_csv(csv)
+        csvs.append(df)
+
+    all_data = aggregate_data(csvs)
+    all_data.to_csv("../data/all.csv")
+    print(all_data)
